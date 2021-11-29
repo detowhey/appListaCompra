7a541619818e344e5440a41b63a8c7fc98c7c263
Henrique de Freitas Almeida <henrique_almeida@terceiros.sicredi.com.br>
2021-11-28 22:38:06 -0300

    Exemplificando a situação

--- app/src/main/java/br/com/qi/activity/MainActivity.kt
+++ app/src/main/java/br/com/qi/activity/MainActivity.kt
@@ -3,9 +3,10 @@ package br.com.qi.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.ListView
-import androidx.core.view.get
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.forEach
 import br.com.qi.R
 import br.com.qi.adapter.AdapterProduto
 import br.com.qi.model.Produto
@@ -15,21 +16,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val listViewItens = findViewById<ListView>(R.id.listViewItens)
         val btnComprar = findViewById<Button>(R.id.btnComprar)
 
         val listaProduto = listOf(
-            Produto(1L, "Feijão 1Kg", 2.69,0),
-            Produto(2L, "Leite", 2.70,0),
-            Produto(3L, "Macarrão", 16.70,0),
-            Produto(4L, "Farofa", 3.38,0),
-            Produto(5L, "Refrigerante", 3.00,0),
-            Produto(6L, "Batata-frita", 5.00,0)
+            Produto(1L, "Feijão 1Kg", 2.69, 0),
+            Produto(2L, "Leite", 2.70, 0),
+            Produto(3L, "Macarrão", 16.70, 0),
+            Produto(4L, "Farofa", 3.38, 0),
+            Produto(5L, "Refrigerante", 3.00, 0),
+            Produto(6L, "Batata-frita", 5.00, 0)
         )
 
         listViewItens.adapter = AdapterProduto(listaProduto, this)
 
+        btnComprar.setOnClickListener {
 
+            val listaPreco = mutableListOf<Double>()
+
+            listViewItens.forEach {
+                if (it.findViewById<TextView>(R.id.txtQuantidade).text.toString().toInt() != 0) {
+                    listaPreco.add(
+                        it.findViewById<TextView>(R.id.txtPreco).text.toString().replace(",", ".")
+                            .substring(
+                                startIndex = 2,
+                                endIndex = findViewById<TextView>(R.id.txtPreco).text.toString().lastIndex
+                            )
+                            .toDouble()
+                    )
+                }
+            }
+
+            var precoTotal = 0.0
+            listaPreco.forEach { numero -> precoTotal += numero }
+
+            Toast.makeText(
+                this,
+                "Valor total: R$ ${String.format("%.2f", precoTotal).replace(".", ",")}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
